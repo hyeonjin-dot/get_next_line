8afa3be9af3492283982aee9ee0e8e297b6acb9c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2021-02-09 14:51:15 +0900

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,86 +6,128 @@
 /*   By: hyejung <hyejung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 19:41:15 by hyejung           #+#    #+#             */
-/*   Updated: 2021/02/01 17:11:43 by hyejung          ###   ########.fr       */
+/*   Updated: 2021/02/04 00:11:53 by hyejung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	gnl_num(char *back)
+int     gnl_num(char *back, int n)
 {
-	int	num;
-
-	num = 0;
-	while (back[num])
-	{
-		if (back[num] == '\n')
-			return (num);
-		num++;
-	}
-	return (-1);
+    int     i;
+    
+    i = 0;
+    while (back[i])
+    {
+        if (back[i] == n)
+            return (i);
+        i++;
+    }
+    return (-1);
 }
 
-int	gnl_split(char **line, char **back)
+int    gnl_new(char **line, char **back)
 {
-	char	*tmp;
-	int		num;
-	int		back_len;
-
-	num = gnl_num(*back);
-	(*back)[num] = '\0';
-	*line = ft_strdup(*back);
-	back_len = ft_strlen(*back + num + 1);
-	if (back_len == 0)
-	{
-		free(*back);
-		*back = 0;
-		return (1);
-	}
-	tmp = ft_strdup(*back + num + 1);
-	free(*back);
-	*back = ft_strdup(tmp);
-	free(tmp);
-	return (1);
+    char    *tmp;
+    int     i;
+    int     idx;
+    size_t  size;
+    
+    if (*back != 0 && (idx = gnl_num(*back, '\n')) >= 0)
+    {
+        (*back)[idx] = '\0';
+        if (!(*line = ft_strdup(*back)))
+            return (-1);
+        
+        if ((size = ft_strlen(*back + idx + 1)) > 0)
+            tmp = ft_strdup(*back + idx + 1);
+        else
+        {
+            free(*back);
+            *back = 0;
+            return (1);
+        }
+        free(*back);
+        if (!(*back = ft_strdup(tmp)))
+            return (-1);
+        free(tmp);
+    }
+    else
+    {
+        i = ((*back == 0) ? 0 : 1);
+        if (!(*line = ((i == 1) ? ft_strdup(*back) : ft_strdup(""))))
+            return (-1);
+        free(*back);
+        *back = NULL;
+        return (0);
+    }
+    return (1);
 }
 
-int	gnl_rt(char **line, char **back)
+int    get_next_line(int fd, char **line)
 {
-	if (*back && (gnl_num(*back) >= 0))
-		return (gnl_split(line, back));
-	else if (*back)
-	{
-		*line = *back;
-		*back = 0;
-		return (0);
-	}
-	*line = ft_strdup("");
-	return (0);
+    static char    *back[OPEN_MAX];
+    char        buf[BUFFER_SIZE + 1];
+    char        *tmp;
+    long        num;
+
+    if (fd < 0 || fd > OPEN_MAX || *line == NULL || BUFFER_SIZE < 1)
+        return (-1);
+    while ((num = read(fd, buf, BUFFER_SIZE)) > 0)
+    {
+        buf[num] = '\0';
+        if (back[fd] == NULL)
+            tmp = ft_strdup(buf);
+        else
+            tmp = ft_strjoin(back[fd], buf);
+        if (tmp == NULL)
+            return (-1);
+        free(back[fd]);
+        back[fd] = ft_strdup(tmp);
+        free(tmp);
+    }
+    return (gnl_new(line, &back[fd]));
 }
 
-int	get_next_line(int fd, char **line)
-{
-	static char	*back[OPEN_MAX];
-	char		buf[BUFFER_SIZE + 1];
-	char		*tmp;
-	int			num;
-	int			idx;
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-	if (back[fd] == NULL)
-		back[fd] = ft_strdup("");
-	while ((num = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[num] = '\0';
-		tmp = ft_strjoin(back[fd], buf);
-		free(back[fd]);
-		back[fd] = ft_strdup(tmp);
-		free(tmp);
-		if ((idx = gnl_num(back[fd])) >= 0)
-			return (gnl_split(line, &back[fd]));
-	}
-	if (num < 0)
-		return (-1);
-	return (gnl_rt(line, &back[fd]));
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+int main()
+{
+    char *new;
+    char *new2;
+
+    int fd;
+    int fd2;
+    int temp;
+    int temp2;
+	int i;
+    
+    
+	i = 1;
+    fd = open("./default_line.txt.output", O_RDONLY);
+    fd2 = open("./65_line", O_RDONLY);
+    
+    
+    new = (char*)malloc(sizeof(char) * 100);
+    new2 = (char*)malloc(sizeof(char) * 100);
+    if (!new || !new2)
+        return (printf("noline \n"));
+    while ((temp = get_next_line(fd, &new)) > 0)
+    {
+        printf("%d:%s\n",temp, new);
+		i++;
+    }
+    printf("%d:%s.\n",temp, new);
+
+	printf("--------------------\n");
+
+   while ((temp2 = get_next_line(fd2, &new2)) > 0)
+    {
+        printf("%d:%s \n",temp2, new2);
+		i++;
+    }
+    printf("%d:%s\n",temp2, new2);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyejung <hyejung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 19:41:34 by hyejung           #+#    #+#             */
-/*   Updated: 2021/01/30 17:49:13 by hyejung          ###   ########.fr       */
+/*   Updated: 2021/02/03 21:12:08 by hyejung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,10 @@
 #  define OPEN_MAX 1024
 # endif
 
-int		get_next_line(int fd, char **line);
-
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+int        get_next_line(int fd, char **line);
+size_t    ft_strlen(const char *str);
+char    *ft_strdup(const char *s);
+char    *ft_strjoin(char const *s1, char const *s2);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
